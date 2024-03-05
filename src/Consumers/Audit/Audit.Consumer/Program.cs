c08551a03da13ac3412b6a25ccd57b5c2172574e
muratc34 <cinekmurat34@gmail.com>
2024-03-05 22:27:51 +0300

    initial commit.

---
+++ src/Consumers/Audit/Audit.Consumer/Program.cs
@@ -0,0 +1,10 @@
+using Audit.Consumer;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
